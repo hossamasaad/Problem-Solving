36b481abdfef896aa6c804c15bb3c90ad0eec13f
hossamasaad <40635600+hossamasaad@users.noreply.github.com>
2019-02-21 20:16:28 +0200

    Create 1.CF - A. Raising Bacteria.cpp

---
+++ Sheet 2/1.CF - A. Raising Bacteria.cpp
@@ -0,0 +1,26 @@
+/*
+ * Hossam Asaad
+ */
+ 
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+ 
+    int neededBact = 0;
+    int x = 0 ;
+    cin >> x;
+ 
+    while(x != 0){
+        if(x%2 != 0){
+            neededBact++;
+        }
+        x = x / 2 ;
+    }
+ 
+    cout << neededBact << endl;
+    return 0;
+}
