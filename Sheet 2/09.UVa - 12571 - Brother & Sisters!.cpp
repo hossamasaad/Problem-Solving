f5965f4811835100d80feee3caa32c567bc47c5e
hossamasaad <40635600+hossamasaad@users.noreply.github.com>
2019-03-14 11:21:10 -0700

    Update 09.UVa - 12571 - Brother & Sisters!.cpp

--- Sheet 2/09.UVa - 12571 - Brother & Sisters!.cpp
+++ Sheet 2/09.UVa - 12571 - Brother & Sisters!.cpp
@@ -1,26 +1,21 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 
     int testCases = 0 ;
+    int N , Q , k , qur ;
+
     cin >> testCases ;
-    while(testCases--){
-        int N=0 , Q=0 ;
+    for(int a = 0 ; a < testCases ; a++){
         cin >> N >> Q ;
         int x[N];
-        for (int i = 0; i < N; ++i) cin >> x[i] ;
-        while(Q--) {
-            int qur = 0;
+        for (int i = 0; i < N; i++) cin >> x[i] ;
+        for(int b=0 ; b < Q ;b++) {
             cin >> qur ;
-            if(qur == 0){
-                cout << 0 << endl;
-                continue;
-            }
-            int k = qur & x[0] ;
-            for (int i = 1; i < N ; ++i) {
-                if(k == x[i]) break;
+            k = qur & x[0] ;
+            for (int i = 1; i < N ; i++) {
+                if(k == qur) break;
                 k = max(k , qur & x[i]) ;
             }
             cout << k << endl;
