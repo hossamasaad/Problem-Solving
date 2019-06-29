16c2cfc46e03929edb262f7f1c292c58d5e3f7a9
hossamasaad <40635600+hossamasaad@users.noreply.github.com>
2019-06-29 17:08:19 +0200

    Create 11231 - Black and white painting.cpp

---
+++ UVA/11231 - Black and white painting.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ll long long
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x,y,c;
+    while(cin >> x >> y >> c && x!=0){
+        cout << ( (x-7)*(y-7) + c ) / 2 << endl;
+    return 0;
+}
