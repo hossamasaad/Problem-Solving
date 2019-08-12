bc59dfe22a40f35040cd90a9931850d5fab56c57
hossamasaad <40635600+hossamasaad@users.noreply.github.com>
2019-08-12 15:36:31 +0200

    Create 2557 - The Drunk Jailer.cpp

---
+++ ICPC Live Aechive/2557 - The Drunk Jailer.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author : Hossam Asaad
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ll long long
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t,n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        cout << (int)sqrt(n) << endl;
+    }
+    return 0;
+}
