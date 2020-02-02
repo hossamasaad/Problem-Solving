e7086e2ff73a063d583054105a21d7e74db015db
hossamasaad <40635600+hossamasaad@users.noreply.github.com>
2020-02-02 20:32:23 +0200

    Update 352 - The Seasonal War.cpp

--- UVA/352 - The Seasonal War.cpp
+++ UVA/352 - The Seasonal War.cpp
@@ -1,54 +1,56 @@
-/*
- * Author : Hossam Asaad
- */
-#include <bits/stdc++.h>
-
-#define ll long long
-#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
-#define REP(i,n) for(int i=1;i<(n);i++)
-#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
-#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#include <iostream>
 
 using namespace std;
 
-char graph[30][30];
-bool visited[30][30];
+char graph[1000][1000];
+bool visited[1000][1000];
+int n;
 
-void visitAround(int i , int j){
-    visited[i][j] = true ;
+int dx[8] = {1,1,1,0,0,-1,-1,-1};
+int dy[8] = {0,1,-1,1,-1,0,1,-1};
 
-    FOR(dx,-1,1)FOR(dy,-1,1)
+void input()
+{  
+    for(int i=0;i<n;i++)
     {
-        if( i==0 && j==0 ) continue;
-        if(graph[i+dx][j+dy]=='1' && !visited[i+dx][j+dy] ) visitAround(i+dx,j+dy);
-        else visited[i+dx][j+dy] = true;
+        for(int j=0;j<n;j++)
+        {
+            cin >> graph[i][j];
+            visited[i][j] = false;
+        }
     }
-
 }
+
+void dfs(int i,int j)
+{
+    if( i<0 || j<0 || i >= n || j >= n) return;
+    if(visited[i][j] || graph[i][j] == '0') return;
+    visited[i][j] = true;
+
+    for(int k=0;k<8;k++)
+    {
+        dfs(i+dx[k],j+dy[k]);
+    }
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n ,ctr=0;
-
-    while(cin >> n){
-        ctr++;
-        int eagles = 0 ;
-        
-        memset(graph,0,sizeof(graph));
-        memset(visited,false,sizeof(visited));
-
-        REP(i,n+1)REP(j,n+1) cin >> graph[i][j];
-
-        REP(i,n+1){
-            REP(j,n+1){
-                if(graph[i][j]=='1' && !visited[i][j]){
-                    visitAround(i,j);
-                    eagles++;
+    int cas = 0;
+    while(cin >> n)
+    {
+        cas++;
+        input();
+        int ctr = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(!visited[i][j] && graph[i][j] == '1')
+                {
+                    dfs(i,j);
+                    ctr++;
                 }
             }
         }
-
-        cout<<"Image number "<<ctr<<" contains "<<eagles<<" war eagles."<<endl;
+        cout << "Image number " << cas <<  " contains " << ctr << " war eagles." << endl;
     }
-    return 0;
 }
